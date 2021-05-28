a539ef0de79a05a8dc114f8b28ff636227727677
Thanh Chiến <70896118+jackychun2002@users.noreply.github.com>
2021-05-28 08:29:22 +0700

    labs6

---
+++ T2008M/labs6/Time.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace T2008M.labs6
+{
+    public class Time
+    {
+        public static void Main(string[] args)
+        {
+            new Thread(delegate(object? obj)
+            {
+                for (int i = 9; i < >= 0; i--)
+                {
+                    for (int j = 59; j >= 0; j--)
+                    {
+                        try
+                        {
+                            Console.WriteLine(i+":" + +j);
+                            Thread.Sleep(100);
+                            Console.Clear();
+                        }
+                        catch (Exception e)
+                        {
+                        }
+                    }
+                }
+                Console.WriteLine("bùm....");
+            })
+                .Start();
+        }
+    }
+}
